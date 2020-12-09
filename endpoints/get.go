be08bee0c61b1336e0a84840f8f3caff601c85f7
Max Garvey <maxgarvey@gmail.com>
2020-12-08 20:42:02 -0800

    Implement get endpoint (#2)
    * Add functionality to get endpoint

--- endpoints/get.go
+++ endpoints/get.go
@@ -4,11 +4,66 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
+
+	"github.com/gomodule/redigo/redis"
+	"github.com/gorilla/mux"
 )
 
+// Response object in redis cache.
+type Response struct {
+	Body      string `json:"body"`
+	ID        string `json:"id"`
+	Status    string `json:"status"`
+	Timestamp int64  `json:"timestamp"`
+}
+
 // Get a response.
-func Get(w http.ResponseWriter, r *http.Request) {
-	// TODO: IMPLEMENT LOGIC
-	fmt.Fprintf(w, "get")
-	log.Printf("get endpoint requested")
+func Get(redisConnection redis.Conn) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// Parse response id from URL.
+		requestID := mux.Vars(r)["id"]
+
+		// Retrieve raw response from DB.
+		rawResponse, err := redis.StringMap(
+			redisConnection.Do(
+				"HGETALL",
+				fmt.Sprintf("response:%s", requestID)))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Parse response.
+		response, err := parseResponse(rawResponse)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		log.Printf(
+			"get endpoint requested. [ID=%s, status=%s]",
+			response.ID,
+			response.Status)
+		// If response is not ready.
+		if response.Status != "DONE" {
+			fmt.Fprintf(w, "not ready")
+			return
+		}
+
+		// If response is ready, return it.
+		fmt.Fprintf(w, response.Body)
+	}
+}
+
+func parseResponse(rawResponse map[string]string) (*Response, error) {
+	var err error
+	response := new(Response)
+	response.Body = rawResponse["body"]
+	response.ID = rawResponse["id"]
+	response.Status = rawResponse["status"]
+	response.Timestamp, err = strconv.ParseInt(rawResponse["timestamp"], 10, 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return response, nil
 }
--- endpoints/get_test.go
+++ endpoints/get_test.go
@@ -1,27 +1,65 @@
 package endpoints
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
+	"github.com/gorilla/mux"
+	"github.com/rafaeljusto/redigomock"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func TestGet(t *testing.T) {
-	// Prelim setup.
+func setup(identifier string, status string, body string) (*httptest.ResponseRecorder, *mux.Router) {
 	recorder := httptest.NewRecorder()
+	redisConnection := redigomock.NewConn()
+	router := mux.NewRouter()
+	router.HandleFunc("/get/{id}", Get(redisConnection))
+
+	// Set up fake data in mock redis.
+	redisConnection.Command(
+		"HGETALL",
+		fmt.Sprintf("response:%s", identifier)).ExpectMap(
+		map[string]string{
+			"id":        identifier,
+			"status":    status,
+			"timestamp": "1607212800", // 12/06/2020 @ 12:00am (UTC)
+			"body":      body,
+		})
+
+	return recorder, router
+}
+
+func TestGetDone(t *testing.T) {
+	recorder, router := setup("done", "DONE", "{\"response\":\"content\"}")
 
 	// Create request.
-	req, err := http.NewRequest("GET", "/get", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
+	req, err := http.NewRequest("GET", "/get/done", nil)
+	require.NoError(t, err)
 
 	// Run request.
-	http.HandlerFunc(Get).ServeHTTP(recorder, req)
+	router.ServeHTTP(recorder, req)
 
 	// Verify response.
 	assert.Equal(t, recorder.Code, http.StatusOK)
-	assert.Equal(t, recorder.Body.String(), "get")
+	assert.Equal(
+		t, "{\"response\":\"content\"}", recorder.Body.String())
+}
+
+func TestGetNotReady(t *testing.T) {
+	recorder, router := setup(
+		"in_progress", "IN_PROGRESS", "{notdoneyet;jsongibberish}")
+
+	// Create request.
+	req, err := http.NewRequest("GET", "/get/in_progress", nil)
+	require.NoError(t, err)
+
+	// Run request.
+	router.ServeHTTP(recorder, req)
+
+	// Verify response.
+	assert.Equal(t, recorder.Code, http.StatusOK)
+	assert.Equal(t, recorder.Body.String(), "not ready")
 }
