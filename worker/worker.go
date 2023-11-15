8fd6f0289bafe6286f1051bae0e5a60e4bb83232
Max Garvey <maxgarvey@gmail.com>
2023-11-15 15:45:37 -0800

    revise worker code and write unit test

--- worker/worker.go
+++ worker/worker.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"log"
 
+	amqp "github.com/rabbitmq/amqp091-go"
+
 	"github.com/maxgarvey/cq_server/data"
 	"github.com/maxgarvey/cq_server/rabbitmq"
 	"github.com/maxgarvey/cq_server/redis"
@@ -27,53 +29,62 @@ func Init(rabbitmq rabbitmq.Rabbit, redis *redis.Redis) *Worker {
 func (w Worker) Work() {
 	go func() {
 		for msg := range w.Rabbitmq.Consume() {
-			fmt.Printf("Received Message: %s\n", msg.Body)
-			thisBody := data.Record{}
-			json.Unmarshal(msg.Body, &thisBody)
-			fmt.Printf("Unmarshalled body: %v\n", thisBody)
-
-			// TODO: handle message: do work
-
-			// Update redis record.
-			token := fmt.Sprintf("response:%s", thisBody.ID)
-			context := context.TODO()
-			redisRecord, err := w.Redis.Get(
-				context,
-				token,
+			w.HandleMessage(
+				msg,
 			)
-			// TODO: handle error in meaningful way.
-			if err != nil {
-				log.Fatalf(
-					"error requesting redis record. err=%v",
-					err,
-				)
-			}
-
-			redisRecord.Status = data.DONE
-			// Marshal updated record
-			updatedRedisRecordJSON, err := json.Marshal(redisRecord)
-			// TODO: handle error in meaningful way.
-			if err != nil {
-				log.Fatalf(
-					"error marshalling updated redis record. err=%v",
-					err,
-				)
-			}
-			// Update redis record
-			err = w.Redis.Set(
-				context,
-				token,
-				updatedRedisRecordJSON,
-			)
-			// TODO: properly handle error.
-			if err != nil {
-				log.Fatalf(
-					"error updating redis record. err=%v",
-					err,
-				)
-			}
 		}
 	}()
 
-	fmt.Println("Waiting for messages...")
+	fmt.Println(
+		"Waiting for messages...",
+	)
+}
+
+func (w Worker) HandleMessage(msg amqp.Delivery) {
+	// Read message.
+	fmt.Printf("Received Message: %s\n", msg.Body)
+	thisBody := data.Record{}
+	json.Unmarshal(msg.Body, &thisBody)
+	fmt.Printf("Unmarshalled body: %v\n", thisBody)
+
+	// Find redis record for this message in redis.
+	token := fmt.Sprintf("response:%s", thisBody.ID)
+	context := context.TODO()
+	redisRecord, err := w.Redis.Get(
+		context,
+		token,
+	)
+	if err != nil {
+		log.Fatalf(
+			"error requesting redis record. err=%v",
+			err,
+		)
+	}
+
+	// TODO: do the work to handle the message.
+
+	// Update the record to update Redis.
+	redisRecord.Status = data.DONE
+
+	// Create JSON to set in Redis
+	updatedRedisRecordJSON, err := json.Marshal(redisRecord)
+	if err != nil {
+		log.Fatalf(
+			"error marshalling updated redis record. err=%v",
+			err,
+		)
+	}
+
+	// Update Redis
+	err = w.Redis.Set(
+		context,
+		token,
+		updatedRedisRecordJSON,
+	)
+	if err != nil {
+		log.Fatalf(
+			"error updating redis record. err=%v",
+			err,
+		)
+	}
 }
