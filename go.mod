340fa9508fb85afb9848c527619a6ef128dfdd9e
Max Garvey <maxgarvey@gmail.com>
2023-11-24 17:19:55 -0800

    init sessions table

--- go.mod
+++ go.mod
@@ -14,11 +14,13 @@ require (
 
 require (
 	github.com/DATA-DOG/go-sqlmock v1.5.0 // indirect
+	github.com/benbjohnson/clock v1.3.5 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/lib/pq v1.10.9 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rabbitmq/amqp091-go v1.9.0 // indirect
+	golang.org/x/crypto v0.15.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- postgres/connection.go
+++ postgres/connection.go
@@ -2,17 +2,23 @@ package postgres
 
 import (
 	"database/sql"
+	"encoding/base64"
 	"fmt"
 	"log/slog"
+	"time"
 
+	"github.com/benbjohnson/clock"
 	_ "github.com/lib/pq"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type Postgres struct {
+	Clock      clock.Clock
 	Connection *sql.DB
+	Logger     slog.Logger
 }
 
-func Init(host string, port int, username string, password string, dbname string, logger slog.Logger) Postgres {
+func Init(host string, port int, username string, password string, dbname string, logger slog.Logger, clock clock.Clock) Postgres {
 	connectionString := fmt.Sprintf(
 		"host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
 		host, port, username, password, dbname,
@@ -33,7 +39,9 @@ func Init(host string, port int, username string, password string, dbname string
 	}
 
 	return Postgres{
+		Clock:      clock,
 		Connection: connection,
+		Logger:     logger,
 	}
 }
 
@@ -64,4 +72,42 @@ func (p *Postgres) UpdateLastLogin(username string) error {
 	}
 
 	return nil
+}
+
+func GenerateToken(user_id int, timestamp string) (string, error) {
+	tokenString := fmt.Sprintf("%d%s", user_id, timestamp)
+	hash, err := bcrypt.GenerateFromPassword(
+		[]byte(tokenString),
+		bcrypt.DefaultCost,
+	)
+	if err != nil {
+		return "", nil
+	}
+
+	return base64.StdEncoding.EncodeToString(hash), nil
+}
+
+func (p *Postgres) CreateSession(user_id int) (string, error) {
+	session_token, err := GenerateToken(
+		user_id,
+		p.Clock.Now().String(),
+	)
+	if err != nil {
+		return "", err
+	}
+
+	if _, err := p.Connection.Exec(
+		"INSERT INTO sessions "+
+			"(user_id, token, created_at, good_until) "+
+			"VALUES "+
+			"($1, $2, $3, $4)",
+		user_id,
+		session_token,
+		p.Clock.Now().String(),
+		p.Clock.Now().Add(time.Hour*24).String(),
+	); err != nil {
+		return "", err
+	}
+
+	return session_token, nil
 }
